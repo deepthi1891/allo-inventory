77b7a405f3c04fa4a210bddd2912f1a398923d4e
Asha deepthi <ddeepthi816@gmail.com>
2026-05-23 15:08:50 +0530

    fix prisma production build

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,3 @@
-export const dynamic = "force-dynamic";
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
@@ -6,8 +5,7 @@ const globalForPrisma = globalThis as unknown as {
 };
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient();
+  globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
